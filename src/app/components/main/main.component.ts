b8e38cbafab983d62027aecf4d6e3ff0df949ab2
ngochieudo <80749502+ngochieudo@users.noreply.github.com>
2022-11-11 03:25:56 +0700

    create slideshow

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -5,6 +5,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  selectedSlide: number = 1;
   constructor() {}
   ngOnInit(): void {}
+  nextSlide() {
+    this.selectedSlide += 1;
+  }
+  previousSlide() {
+    this.selectedSlide -= 1;
+  }
 }
